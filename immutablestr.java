1679dc1fba45f5881a53fff6dad809055672c62e
devendratill <devapatil21011@gmail.com>
2023-07-31 18:28:14 +0530

    Immutable String and practice memory maping

---
+++ immutablestr.java
@@ -0,0 +1,27 @@
+/**
+ * demo
+ */
+public class immutablestr {
+
+ public static void main(String[] args) {
+//     String name="deva";
+//     String name1="dev";
+//     System.out.println(name==name1);
+
+//     String nam=new String("deva");
+//     String nam1=new String("deva");
+//     System.out.println(nam==nam1);
+
+//     System.out.println(name==nam);
+//     System.out.println(name.equals(nam));
+String a1="java";
+String a2="java";
+String a3="Java";
+
+System.out.println(a1==a2);
+System.out.println(a1==a3);
+System.out.println(a1.equals(a2));
+System.out.println(a1.equalsIgnoreCase(a3));
+
+   }
+}
